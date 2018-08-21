f06ca2ec7e85e49c62f9f788b0475921dca882f7
Eric Smith <esmith@8thlight.com>
2018-08-21 13:09:59 -0500

    Add a near empty mastermind CPlusPlus setup

---
+++ CPlusPlus/MasterMind/mastermind.tests.cpp
@@ -0,0 +1,10 @@
+// For more on how to use the excellent Catch test framework see
+// https://github.com/philsquared/Catch
+
+#include "catch.hpp"
+
+TEST_CASE( "Life the universe and everything", "[hhgttg]" )
+{
+    REQUIRE( 43 == 42 );
+}
+
